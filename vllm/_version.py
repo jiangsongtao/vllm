c276e06c009a48d2a7e1321884af5a11e2fb5d64
Eric Jiang <43131870+jiangsongtao@users.noreply.github.com>
2025-11-27 13:33:16 +0800

    Add files via upload
    Signed-off-by: Eric Jiang <43131870+jiangsongtao@users.noreply.github.com>

---
+++ vllm/_version.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+__version__ = "0.11.2+hulumed"
+__version_tuple__ = (0, 11, 2)
+
+def _get_version():
+    return __version__
+
+# For compatibility
+version = __version__
+version_tuple = __version_tuple__
